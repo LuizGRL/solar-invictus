2a486ff10b170d18960e34f30e9a8cdd7cec0154
snork <luizguilherme.lgrl@gmail.com>
2025-03-16 15:11:50 -0300

    nav bar funcionando

---
+++ assets/js/scroll.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();  
+
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth', 
+            block: 'start'      
+        });
+    });
+});
